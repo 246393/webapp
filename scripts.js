d103ad981b33a14dd91d10651b324d0a0302bf84
246393 <98038194+246393@users.noreply.github.com>
2024-05-06 11:10:53 -0500

    5/6

---
+++ scripts.js
@@ -0,0 +1,24 @@
+//select the button element
+
+
+// Add event listener for the 'click' event
+document.getElementById('podcastB').addEventListener('click', function() {
+    window.location.href = 'podcast.html';
+})
+
+document.getElementById('favB').addEventListener('click', function() {
+    window.location.href = 'fav.html';
+})
+
+document.getElementById('studyB').addEventListener('click', function() {
+    window.location.href = 'study.html';
+})
+
+document.getElementById('supriseB').addEventListener('click', function() {
+    window.location.href = 'suprise.html';
+})
+
+document.getElementById('home').addEventListener('click', function() {
+    window.location.href = 'index.html';
+})
+
