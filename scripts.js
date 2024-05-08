8e00885516b31a83ee434505b1af3fd70f92edb1
246393 <98038194+246393@users.noreply.github.com>
2024-05-08 11:09:23 -0500

    5/8

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 //select the button element
 
 
-// Add event listener for the 'click' event
+/* Add event listener for the 'click' event
 document.getElementById('podcastB').addEventListener('click', function() {
     window.location.href = 'podcast.html';
 })
@@ -18,7 +18,33 @@ document.getElementById('supriseB').addEventListener('click', function() {
     window.location.href = 'suprise.html';
 })
 
+
+//Issues with home button
 document.getElementById('home').addEventListener('click', function() {
     window.location.href = 'index.html';
 })
 
+*/
+
+function showPanel1(){
+    document.getElementById("panel-1").style.display = "block";
+}
+
+function showPanel2(){
+    document.getElementById("panel-2").style.display = "block";
+}
+
+function showPanel3(){
+    document.getElementById("panel-3").style.display = "block";
+}
+
+function showPanel4(){
+    document.getElementById("panel-4").style.display = "block";
+}
+
+function hide(){
+    panels=document.getElementsByClassName("panel");  // Find the elements
+        for(var i = 0; i < panels.length; i++){
+            panels[i].style.display = "none";    // Change the content
+    }
+}
