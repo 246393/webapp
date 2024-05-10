3ce5665934c475494a3aff19f441d4719a537c93
246393 <98038194+246393@users.noreply.github.com>
2024-05-10 10:14:05 -0500

    5/10

--- scripts.js
+++ scripts.js
@@ -1,30 +1,4 @@
-//select the button element
 
-
-/* Add event listener for the 'click' event
-document.getElementById('podcastB').addEventListener('click', function() {
-    window.location.href = 'podcast.html';
-})
-
-document.getElementById('favB').addEventListener('click', function() {
-    window.location.href = 'fav.html';
-})
-
-document.getElementById('studyB').addEventListener('click', function() {
-    window.location.href = 'study.html';
-})
-
-document.getElementById('supriseB').addEventListener('click', function() {
-    window.location.href = 'suprise.html';
-})
-
-
-//Issues with home button
-document.getElementById('home').addEventListener('click', function() {
-    window.location.href = 'index.html';
-})
-
-*/
 
 function showPanel1(){
     document.getElementById("panel-1").style.display = "block";
